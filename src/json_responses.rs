6c89da2cbf96aa6d100820e1e80e429f349e858d
Gianluca Fabrizi <info@gianlucafabrizi.it>
2020-03-04 23:39:08 +0100

    Refactoring json_responses.rs

--- src/json_responses.rs
+++ src/json_responses.rs
@@ -3,14 +3,12 @@ use serde::ser::SerializeStruct;
 
 use rocket::response::status;
 use rocket::http::Status;
-use rocket_contrib::json;
 use rocket_contrib::json::{Json, JsonValue};
 
 use crate::models::User;
 
 pub enum JsonResponses<'a> {
     JsonSuccessResponse(JsonSuccessResponse<'a>),
-    JsonSuccessNullDataResponse(JsonSuccessNullDataResponse<'a>),
     JsonFailResponse(JsonFailResponse<'a>),
     JsonErrorResponse(JsonErrorResponse<'a>),
 }
@@ -28,20 +26,15 @@ impl Serialize for JsonResponses<'_> {
                 s.serialize_field("status", resp.status)?;
                 s.serialize_field("data", &resp.data)?;
             },
-            JsonResponses::JsonSuccessNullDataResponse(resp) => {
-                s = serializer.serialize_struct("JsonSuccessNullDataResponse", 2)?;
-                s.serialize_field("status", resp.status)?;
-                s.serialize_field("data", &resp.data)?;
-            },
             JsonResponses::JsonFailResponse(resp) => {
                 s = serializer.serialize_struct("JsonFailResponse", 2)?;
                 s.serialize_field("status", resp.status)?;
-                s.serialize_field("data", resp.data)?;
+                s.serialize_field("data", &resp.data)?;
             },
             JsonResponses::JsonErrorResponse(resp) => {
                 s = serializer.serialize_struct("JsonErrorResponse", 2)?;
                 s.serialize_field("status", resp.status)?;
-                s.serialize_field("message", resp.message)?;
+                s.serialize_field("message", &resp.message)?;
             }
         };
 
@@ -52,25 +45,19 @@ impl Serialize for JsonResponses<'_> {
 #[derive(Serialize)]
 pub struct JsonSuccessResponse<'a> {
     status: &'a str,
-    data: Payload,
-}
-
-#[derive(Serialize)]
-pub struct JsonSuccessNullDataResponse<'a> {
-    status: &'a str,
-    data: JsonValue,
+    data: ResponsePayload<'a>,
 }
 
 #[derive(Serialize)]
 pub struct JsonFailResponse<'a> {
     status: &'a str,
-    data: &'a str,
+    data: ResponsePayload<'a>,
 }
 
 #[derive(Serialize)]
 pub struct JsonErrorResponse<'a> {
     status: &'a str,
-    message: &'a str,
+    message: ResponsePayload<'a>,
 }
 
 #[derive(Serialize)]
@@ -79,46 +66,69 @@ pub enum Payload {
     Users(Vec<User>),
 }
 
-pub fn json_success_response<'a>(data: Payload, status_code: Status) -> status::Custom<Json<JsonResponses<'a>>> {
-    let result = JsonResponses::JsonSuccessResponse(
-        JsonSuccessResponse {
-            status: "success",
-            data
-        }
-    );
-
-    status::Custom(status_code, Json(result))
+pub enum ResponsePayload<'a> {
+    Payload(Payload),
+    JsonValue(JsonValue),
+    Slice(&'a str),
 }
 
-pub fn json_success_response_null_data<'a>(status_code: Status) -> status::Custom<Json<JsonResponses<'a>>> {
-    let result = JsonResponses::JsonSuccessNullDataResponse(
-        JsonSuccessNullDataResponse {
-            status: "success",
-            data: json!(null)
-        }
-    );
-
-    status::Custom(status_code, Json(result))
+impl Serialize for ResponsePayload<'_> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+    {
+        return match self {
+            ResponsePayload::Payload(p) => {
+                let mut s = serializer.serialize_struct("Payload", 1)?;
+                match p {
+                    Payload::User(u) => s.serialize_field("user", u)?,
+                    Payload::Users(u) => s.serialize_field("users", u)?,
+                }
+                s.end()
+            },
+            ResponsePayload::JsonValue(j) => {
+                j.0.serialize(serializer)
+            },
+            ResponsePayload::Slice(str) => {
+                serializer.serialize_str(str)
+            }
+        };
+    }
 }
 
-pub fn json_error_response(message: &str, status_code: Status) -> status::Custom<Json<JsonResponses>> {
-    let result = JsonResponses::JsonErrorResponse(
-        JsonErrorResponse {
-            status: "error",
-            message
-        }
-    );
-
-    status::Custom(status_code, Json(result))
+pub enum ResponseType {
+    Success,
+    Fail,
+    Error,
 }
 
-pub fn json_fail_response(data: &str, status_code: Status) -> status::Custom<Json<JsonResponses>> {
-    let result = JsonResponses::JsonFailResponse(
-        JsonFailResponse {
-            status: "fail",
-            data
+pub fn json_response(payload: ResponsePayload, status_code: Status, response_type: ResponseType) -> status::Custom<Json<JsonResponses>> {
+    let response = match response_type {
+        ResponseType::Success => {
+            JsonResponses::JsonSuccessResponse(
+                JsonSuccessResponse {
+                    status: "success",
+                    data: payload,
+                }
+            )
+        },
+        ResponseType::Fail => {
+            JsonResponses::JsonFailResponse(
+                JsonFailResponse {
+                    status: "fail",
+                    data: payload,
+                }
+            )
+        },
+        ResponseType::Error => {
+            JsonResponses::JsonErrorResponse(
+                JsonErrorResponse {
+                    status: "error",
+                    message: payload,
+                }
+            )
         }
-    );
+    };
 
-    status::Custom(status_code, Json(result))
+    status::Custom(status_code, Json(response))
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket_contrib::json;
 use rocket_contrib::json::Json;
 use rocket::response::status;
 use rocket::http::Status;
@@ -23,8 +24,14 @@ fn list_users<'a>() -> status::Custom<Json<JsonResponses<'a>>> {
     let connection = establish_connection();
 
     match users.load::<User>(&connection) {
-        Ok(u) => json_success_response(Payload::Users(u), Status::Ok),
-        Err(_) => json_error_response("An error occurred while loading records", Status::InternalServerError)
+        Ok(u) => json_response(
+            ResponsePayload::Payload(Payload::Users(u)),
+            Status::Ok,
+            ResponseType::Success),
+        Err(_) => json_response(
+            ResponsePayload::Slice("An error occurred while loading records"),
+            Status::InternalServerError,
+            ResponseType::Error)
     }
 }
 
@@ -36,8 +43,14 @@ fn create_user<'a>(user: Json<User>) -> status::Custom<Json<JsonResponses<'a>>> 
     let user = user.into_inner();
 
     match do_create_user(&connection, &user) {
-        Ok(_) => json_success_response_null_data(Status::Created),
-        Err(_) => json_error_response("An error occurred while creating the record", Status::InternalServerError)
+        Ok(_s) => json_response(
+            ResponsePayload::JsonValue(json!(null)),
+            Status::Created,
+            ResponseType::Success),
+        Err(_) => json_response(
+            ResponsePayload::Slice("An error occurred while creating the record"),
+            Status::InternalServerError,
+            ResponseType::Error)
     }
 }
 
@@ -48,8 +61,14 @@ fn retrieve_user<'a>(uid: i32) -> status::Custom<Json<JsonResponses<'a>>> {
 
     match users.find(uid)
         .first::<User>(&connection) {
-        Ok(u) => json_success_response(Payload::User(u), Status::Ok),
-        Err(_) => json_fail_response("Record not found", Status::NotFound)
+        Ok(u) => json_response(
+            ResponsePayload::Payload(Payload::User(u)),
+            Status::Ok,
+            ResponseType::Success),
+        Err(_) => json_response(
+            ResponsePayload::Slice("Record not found"),
+            Status::NotFound,
+            ResponseType::Fail)
     }
 }
 
@@ -62,12 +81,21 @@ fn update_user<'a>(uid: i32, user: Json<User>) -> status::Custom<Json<JsonRespon
 
     let user_to_update = match users.find(uid).first::<User>(&connection) {
         Ok(u) => u,
-        Err(_) => return json_fail_response("Record not found", Status::NotFound)
+        Err(_) => return json_response(
+            ResponsePayload::Slice("Record not found"),
+            Status::NotFound,
+            ResponseType::Fail)
     };
 
     match do_update_user(&connection, &user_to_update, &new_data) {
-        Ok(_) => json_success_response_null_data(Status::Ok),
-        Err(_) => json_error_response("An error occurred while updating the record", Status::InternalServerError)
+        Ok(_s) => json_response(
+            ResponsePayload::JsonValue(json!(null)),
+            Status::Ok,
+            ResponseType::Success),
+        Err(_) => json_response(
+            ResponsePayload::Slice("An error occurred while updating the record"),
+            Status::InternalServerError,
+            ResponseType::Error)
     }
 }
 
@@ -78,12 +106,21 @@ fn delete_user<'a>(uid: i32) -> status::Custom<Json<JsonResponses<'a>>> {
 
     if let Err(_) = users.find(uid)
         .first::<User>(&connection) {
-        return json_fail_response("Record not found", Status::NotFound)
+        return json_response(
+            ResponsePayload::Slice("Record not found"),
+            Status::NotFound,
+            ResponseType::Fail)
     };
 
     match do_delete_user(&connection, uid) {
-        Ok(_) => json_success_response_null_data(Status::Ok),
-        Err(_) => json_error_response("An error occurred while deleting the record", Status::InternalServerError)
+        Ok(_s) => json_response(
+            ResponsePayload::JsonValue(json!(null)),
+            Status::Ok,
+            ResponseType::Success),
+        Err(_) => return json_response(
+            ResponsePayload::Slice("An error occurred while deleting the record"),
+            Status::InternalServerError,
+            ResponseType::Error)
     }
 }
 
